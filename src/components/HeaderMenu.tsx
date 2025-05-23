8556f0bcf0053f6efa04002f3a2743eee95e0d43
DaniilT048 <tis.daniil98@gmail.com>
2025-05-23 16:04:04 +0300

    routes working

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -2,14 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import type {JSX} from "react";
-import routes from "../routes.ts";
+import routes from "../routes.tsx";
 
 function HeaderMenu(): JSX.Element {
     return (
         <>
             <Navbar bg="primary" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="/"><img src="../../public/sport-icon.svg" alt="sport icon"/></Navbar.Brand>
+                    <Navbar.Brand href="/">check</Navbar.Brand>
                     <Nav className="me-auto">
                         {routes.map((route) =>
                             <li key={route.path}>
