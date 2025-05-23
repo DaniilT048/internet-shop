5a7fcb51343d17c60da152cd71eb9e9e3dd8971c
DaniilT048 <tis.daniil98@gmail.com>
2025-05-23 18:26:16 +0300

    maked products data, and maked shop page and card

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -3,20 +3,18 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ShopCard from "./ShopCard.tsx";
+import products from "../data/products.ts";
 
 const Shop = ():ReactElement => {
     document.title = "Shop";
     return(
         <Container>
             <Row md={4}>
-                <Col><ShopCard/></Col>
-                <Col><ShopCard/></Col>
-                <Col><ShopCard/></Col>
-                <Col><ShopCard/></Col>
-                <Col><ShopCard/></Col>
-                <Col><ShopCard/></Col>
-                <Col><ShopCard/></Col>
-                <Col><ShopCard/></Col>
+                {products.map((product) => (
+                    <Col key={product.id} sm={6} md={4} lg={3}>
+                        <ShopCard product={product} />
+                    </Col>
+                ))}
             </Row>
         </Container>
     )
--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -1,17 +1,20 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import  { type Product } from '../data/products';
+import type {JSX} from "react";
 
-function ShopCard() {
+export type ProductProps = {
+    product: Product;
+};
+
+function ShopCard({ product }: ProductProps): JSX.Element {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+        <Card >
+            <Card.Img variant="top" src={product.image} alt={product.name} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>${product.price.toFixed(2)}</Card.Text>
+                <Button variant="primary">Add to Cart</Button>
             </Card.Body>
         </Card>
     );
