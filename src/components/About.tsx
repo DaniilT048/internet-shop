57f61f214a05dac25d52be33662ac1c69d9b71ab
DaniilT048 <tis.daniil98@gmail.com>
2025-06-06 11:45:42 +0300

    maked about page, and links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,25 @@
+import Container from "react-bootstrap/Container";
+import {Link} from "react-router-dom";
 import type {ReactElement} from "react";
 
 const About = ():ReactElement => {
     document.title = "About";
     return(
-        <div>
+        <Container>
             <h1>
                 About Page
             </h1>
-        </div>
+            <p>
+                Whether you're building a home gym, training for a competition, or just starting your fitness journey — we've got you covered.
+                From dumbbells and resistance bands to professional gym machines, our wide range of products meets the needs of athletes, beginners, and fitness enthusiasts alike.
+            </p>
+            <ul>
+                <li>We are have <Link to="/products?category=Balls" className="link-primary">Balls</Link></li>
+                <li>We are have <Link to="/products?category=Dumbbells" className="link-primary">Dumbbells</Link></li>
+                <li>We are have <Link to="/products?category=Mats" className="link-primary">Mats</Link></li>
+                <li>We are have <Link to="/products?category=Accessories" className="link-primary">Accessories</Link></li>
+            </ul>
+        </Container>
     )
 
 }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,22 +4,29 @@ import Form from 'react-bootstrap/Form';
 import products from "../data/products.ts";
 import ItemsList from "./ItemsList.tsx";
 import {Category} from "../types/Category.ts";
-
-
+import {useLocation, useNavigate} from "react-router";
 
 
 const Products = ():ReactElement => {
     document.title = "Shop";
-    const [category, setCategory] = useState<Category>(Category.ALL);
+
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const categoryFromUrl = (queryParams.get("category") as Category) || Category.ALL;
+    const navigate = useNavigate();
+
+    const [category, setCategory] = useState<Category>(categoryFromUrl);
 
     const handleCategoryChange= (event: ChangeEvent<HTMLSelectElement>)=> {
-        setCategory(event.target.value as Category);
+        const selectedCategory = event.target.value as Category;
+        setCategory(selectedCategory);
+        navigate(`/products?category=${selectedCategory}`);
     }
     const filteredProducts = category === Category.ALL ? products : products.filter(product => product.category === category);
     return(
         <Container>
             <h4>Chose category: {category}</h4>
-            <Form.Select className="mb-4" onChange={handleCategoryChange}>
+            <Form.Select className="mb-4" onChange={handleCategoryChange} value={category}>
                 <option value={Category.ALL}>All products</option>
                 <option value={Category.BALLS}>Balls</option>
                 <option value={Category.DUMBBELLS}>Dumbbells</option>
