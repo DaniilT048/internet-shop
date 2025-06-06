7211045c45020c8841c986491ec90ba2f766fe3f
DaniilT048 <tis.daniil98@gmail.com>
2025-06-06 11:15:00 +0300

    maked localStorage

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -8,9 +8,14 @@ type CartItem = {
 type CartState = {
     items: CartItem[];
 }
+
+const initialCart = JSON.parse(localStorage.getItem("cart") || "[]");
+
 const initialState: CartState = {
-    items: [],
+    items: initialCart,
 }
+
+
 const cartSlice = createSlice({
     name: "cart",
     initialState,
@@ -20,27 +25,33 @@ const cartSlice = createSlice({
             if (existingCart) {
                 existingCart.quantity++
                 console.log(existingCart.quantity);
+
             }else{
                 state.items.push({id: action.payload, quantity: 1});
             }
+            localStorage.setItem("cart", JSON.stringify(state.items));
         },
         removeFromCart: (state, action: PayloadAction<number>) => {
                 state.items = state.items.filter(item => item.id !== action.payload);
+                localStorage.setItem("cart", JSON.stringify(state.items));
         },
         incrementQty: (state, action: PayloadAction<number>) => {
             const item = state.items.find(item => item.id === action.payload);
             if (item) {
                 item.quantity++;
+                localStorage.setItem("cart", JSON.stringify(state.items));
             }
         },
         decrementQty: (state, action: PayloadAction<number>) => {
             const item = state.items.find(item => item.id === action.payload);
             if (item && item.quantity > 1) {
                 item.quantity--;
+                localStorage.setItem("cart", JSON.stringify(state.items));
             }
         },
         clearCart(state){
             state.items =[];
+            localStorage.removeItem("cart");
         }
     }
 })
