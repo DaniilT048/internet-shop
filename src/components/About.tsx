60d2811d670321bf31fb90272a06f99b50247d88
DaniilT048 <tis.daniil98@gmail.com>
2025-06-06 14:33:37 +0300

    path in about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,25 +15,25 @@ const About = ():ReactElement => {
                 needs of athletes, beginners, and fitness enthusiasts alike.
             </p>
             <div className="list-group">
-                <a href="/products?category=Balls" className="list-group-item list-group-item-action flex-column align-items-start">
+                <a href="./products?category=Balls" className="list-group-item list-group-item-action flex-column align-items-start">
                     <div className="d-flex w-100 justify-content-between">
                         <h5 className="mb-1">Balls</h5>
                     </div>
                     <p className="mb-1">We have a lot of balls</p>
                 </a>
-                <a href="/products?category=Dumbbells" className="list-group-item list-group-item-action flex-column align-items-start">
+                <a href="./products?category=Dumbbells" className="list-group-item list-group-item-action flex-column align-items-start">
                     <div className="d-flex w-100 justify-content-between">
                         <h5 className="mb-1">Dumbbells</h5>
                     </div>
                     <p className="mb-1">We have a lot of dumbbells</p>
                 </a>
-                <a href="/products?category=Mats" className="list-group-item list-group-item-action flex-column align-items-start">
+                <a href="./products?category=Mats" className="list-group-item list-group-item-action flex-column align-items-start">
                     <div className="d-flex w-100 justify-content-between">
                         <h5 className="mb-1">Mats</h5>
                     </div>
                     <p className="mb-1">We have a lot of mats</p>
                 </a>
-                <a href="/products?category=Accessories" className="list-group-item list-group-item-action flex-column align-items-start">
+                <a href="./products?category=Accessories" className="list-group-item list-group-item-action flex-column align-items-start">
                     <div className="d-flex w-100 justify-content-between">
                         <h5 className="mb-1">Accessories</h5>
                     </div>
